315ed45471ca8141654a0196f99f91c71db748a0
Matheus Barbosa <matts.barbosa@gmail.com>
2023-06-13 22:29:59 -0300

    criando e estilizando os componentes e containers

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,31 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+const ListaDeTarefas = () => (
+  <>
+    <Container>
+      <p>
+        2 tarefas marcadas como: &quot;categorio&ldquo; e &quot;termo&ldquo;{' '}
+      </p>
+      <ul>
+        <li>
+          <Tarefa />
+        </li>
+        <li>
+          <Tarefa />
+        </li>
+        <li>
+          <Tarefa />
+        </li>
+        <li>
+          <Tarefa />
+        </li>
+        <li>
+          <Tarefa />
+        </li>
+      </ul>
+    </Container>
+  </>
+)
+
+export default ListaDeTarefas
